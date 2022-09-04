c3b8abe4184dea87b014a8c19ddaf87ea757e2ef
Erlend <erlend_hapnes@hotmail.com>
2022-09-04 21:26:01 +0200

    increased asnlookupinit malloc size

--- libparistraceroute/sniffer.c
+++ libparistraceroute/sniffer.c
@@ -630,7 +630,6 @@ void sniffer_process_packets(sniffer_t *sniffer, uint8_t protocol_id)
 
                     t = createTraceroute();
                     puts("createTraceroute done");
-                    t->outgoing_tcp_port;
                     set_traceroute(t);
                     puts("set_traceroute done");
                     t->timestamp = create_timestamp();
