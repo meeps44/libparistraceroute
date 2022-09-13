71fcc5628aa1f2b080e985fd3e8eb66311a3b0c2
Erlend <erlend_hapnes@hotmail.com>
2022-09-13 18:51:51 +0200

    Added debug statements

--- libparistraceroute/network.c
+++ libparistraceroute/network.c
@@ -764,19 +764,31 @@ bool network_process_recvq(network_t *network)
     probe_reply_set_reply(probe_reply, reply);
 
     // BEGIN ERLEND //
+    fprintf(stderr, "Entering get_traceroute\n");
     traceroute *t = get_traceroute();
+    fprintf(stderr, "Finished get_traceroute\n");
+    fprintf(stderr, "Entering probe parse_ipv6\n");
     ipv6_header *probe_ipv6 = parse_ipv6(probe->packet->buffer->data);
+    fprintf(stderr, "Finished probe parse_ipv6\n");
+    fprintf(stderr, "Entering outer parse_ipv6\n");
     ipv6_header *outer_ipv6 = parse_ipv6(reply->packet->buffer->data);
+    fprintf(stderr, "Finished outer parse_ipv6\n");
+    fprintf(stderr, "Entering inner parse_ipv6\n");
     ipv6_header *inner_ipv6 = get_inner_ipv6_header(reply->packet->buffer->data);
+    fprintf(stderr, "Finished inner parse_ipv6\n");
     uint32_t returned_flowlabel = inner_ipv6->flow_label;
 
+    fprintf(stderr, "Entering createHop\n");
     hop *h = createHop();
+    fprintf(stderr, "Finished createHop\n");
     h->hopnumber = probe_ipv6->hop_limit;
     h->hop_address = outer_ipv6->source;
     h->returned_flowlabel = returned_flowlabel;
 
     /* Set hop ASN */
+    fprintf(stderr, "Entering asnLookup\n");
     char *asnlookup_result = asnLookup(&h->hop_address);
+    fprintf(stderr, "Finished asnLookup\n");
     if (asnlookup_result != NULL)
     {
         memcpy(h->hop_asn, asnlookup_result, strlen(asnlookup_result) + 1);
@@ -785,12 +797,16 @@ bool network_process_recvq(network_t *network)
     {
         strcpy(h->hop_asn, "NULL");
     }
+    fprintf(stderr, "Entering printHop\n");
     printHop(h);
+    fprintf(stderr, "Finished printHop\n");
 
+    fprintf(stderr, "Entering appendHop\n");
     if (appendHop(h, t) == -1)
     {
         fprintf(stderr, "Failed to append hop: Hop array is full\n");
     }
+    fprintf(stderr, "Finished appendHop\n");
     // END ERLEND //
 
     // Notify the instance which has build the probe that we've got the corresponding reply
